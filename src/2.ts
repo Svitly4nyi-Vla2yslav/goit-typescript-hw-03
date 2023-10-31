d1a6df27d771d48c9e1b9a7c7d7a92aa4af425ea
Svitly4nyi-Vla2yslav <126347711+Svitly4nyi-Vla2yslav@users.noreply.github.com>
2023-11-01 00:00:00 +0100

    fix

--- src/2.ts
+++ src/2.ts
@@ -28,8 +28,8 @@ class Employee {
 
 class Manager extends Employee {
   // Реалізуйте конструктор та збільшіть salary на 10000
-  constructor(name: string, depart: string, salary: number) {
-    super(name, depart, salary + 10000)
+  constructor(name: string, department: string, salary: number) {
+    super(name, department, salary + 10000)
   }
 }
 
